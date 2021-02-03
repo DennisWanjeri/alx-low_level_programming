0e15d0bb76077cc79ea3a09b7833c78a159a123d
root <shikandadennis07@gmail.com>
2021-02-03 19:29:43 +0000

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main - entry point
  *
  *Return: value 1
  */
-int main()
+int main(void)
 {
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+return (1);
 
 }
