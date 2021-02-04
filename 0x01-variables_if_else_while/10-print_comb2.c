f479ef2311bcf559692fb1f0c350c0be8a12936b
root <shikandadennis07@gmail.com>
2021-02-04 19:33:16 +0000

    files

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description:prints number from 00 to 99
+ *
+ *Return:always zero
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = 0;dig < 100;dig++)
+	{
+		putchar((dig / 10)+ '0');
+		putchar((dig % 10)+ '0');
+		if (dig < 99)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description:prints all possible combinations of single-digit no.
+ *
+ *Return: Always zero
+ */
+int main()
+{
+	int dig, i;
+
+	for (dig = 48,i = 0;i < 10; dig++, i++)
+	{
+		putchar(num);
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		putchar(10);
+		return (0);
+
+}
