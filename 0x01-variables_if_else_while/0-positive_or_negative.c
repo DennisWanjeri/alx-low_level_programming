3164eb86af8e19571cf5c008264b3282a643030b
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-05 20:27:45 +0000

    done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -27,5 +27,6 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
+	}
 	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("last digit of %d is %d and is greater than 5\n", n, last);
 	}
-	else if (last < 6 && last =! 0)
+	else if (last < 6 && last != 0)
 	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
