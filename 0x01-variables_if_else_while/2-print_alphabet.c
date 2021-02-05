714ae01565c3ff0e89cf6519c5c747dbf577e6eb
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-05 20:18:14 +0000

    debug

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,14 +8,12 @@
  */
 int main(void)
 {
-	int letter,n;
+	int letter, n;
 
-	for (letter = 97,n = 1;n<27;letter++,n++)
+	for (letter = 97, n = 1 ; n < 27; letter++, n++)
 	{
 		putchar(letter);
 	}
 	putchar(10); 
      	return (0);
-
-
 }
