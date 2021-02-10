9626afeb087121af187045fad915449e927809bd
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-10 11:07:45 +0000

    debugged

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,21 +28,21 @@ void print_times_table(int n)
 				}
 				else if (result >= 10)
 				{
-					_putchar(" ");
+					_putchar(' ');
 					_putchar('0' + (result / 10));
 					_putchar('0' + (result % 10));
 				}
 				else if (result >= 100)
 				{
-					_putchar(" ");
+					_putchar(' ');
 					_putchar('0' + (result / 100));
 					_putchar('0' + ((result / 10) % 10));
 					_putchar('0' + (result % 10));
 				}
 				else
 				{
-					_putchar(" ");
-					_putchar(" ");
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(result + '0');
 				}
 				if (j != n)
