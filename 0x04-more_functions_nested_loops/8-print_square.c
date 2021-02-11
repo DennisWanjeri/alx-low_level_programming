842b5638262a1c7da55cdf0ca17dcd010b41d7f8
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-11 11:53:17 +0000

    debugging

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,11 +13,11 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
 			int j;
 
-			for (j = 0; j <= size; j++)
+			for (j = 1; j <= size; j++)
 			{
 				if (j == size)
 				{
