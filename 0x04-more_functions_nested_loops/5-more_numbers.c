92be51e4f487d3757e64e70160058d4e8acff18c
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-11 11:34:40 +0000

    debug

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,7 +16,15 @@ void more_numbers(void)
 
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar('0' + (j / 10));
+				_putchar('0' + (j % 10));
+			}
+			else
+			{
+				_putchar('0' + j);
+			}
 		}
 		_putchar('\n');
 	}
