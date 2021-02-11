cfda12fc6122a7782848d8c1c3c2402621399e51
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-11 05:04:54 +0000

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *print_numbers - print from 0-9 + newline
+ *
+ *Description:0-9
+ *
+ *Return:void
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
