32eb03b69f289dbceb2a12dd41e1423db10ec5b8
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-11 12:42:45 +0000

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,5 +9,25 @@
  */
 void print_triangle(int size)
 {
+	int i, j, gap;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (gap = 1; gap <= (size - 1); gap++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
