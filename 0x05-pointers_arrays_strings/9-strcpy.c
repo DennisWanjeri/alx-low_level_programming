3226636e86423199e42a2c0eb1eab977f9a29099
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-16 13:42:17 +0000

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,12 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	for (i = 0; *src != '\0'; i++)
+
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		dest[i] = src[i];
 	}
-	*dest = '\0';
+	dest[i] = '\0';
+	return (dest);
 
 }
