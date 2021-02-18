ebe05450dc6e4bd913249209e8d562c46d543903
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-18 07:48:33 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+/**
+ *_strncpy - copies a string
+ *
+ *@dest:destination
+ *@src:source
+ *@n:range
+ *
+ *Return:dest
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *start = dest;
+
+	while (*src && n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
