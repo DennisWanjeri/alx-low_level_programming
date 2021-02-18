5d63435cd00a52bdf6f6c275dcdd8f15b19b3639
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-18 16:38:09 +0000

    debug

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *start = dest;
+	int i;
 
-	while (*src && n--)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[i];
 	}
-	*dest = '\0';
-	return (start);
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }
