7c6b96c2109223c8716c183c5990f6011a081cd7
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-18 16:19:06 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, destlength, j;
+	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
 	{
-		destlength++;
+		i++;
 	}
-	for (j = 0; src[j] != '\0'; j++, destlength++)
+	while (src[i] != '\0')
 	{
-		dest[destlength] = src[j];
+		dest[i] = src[i];
+		j++;
+		i++;
 	}
-	dest[destlength] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
