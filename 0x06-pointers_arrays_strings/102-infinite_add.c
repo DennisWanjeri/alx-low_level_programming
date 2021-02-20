a4225f0ce56267588dc76ee934933a0668b2f740
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-20 08:35:28 +0000

    overflow error

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -74,6 +74,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			val2 = *(n2 + j) - '0';
 		}
 		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+		{
+			overflow = 1;
+		}
+		else
+		{
+			overflow = 0;
+		}
 		if (digits >= (size_r - 1))
 		{
 			return (0);
