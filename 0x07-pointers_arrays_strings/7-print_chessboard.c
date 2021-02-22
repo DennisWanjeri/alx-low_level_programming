5ec8b17e9a8fbaf292b76e09e10736b0533c5f4b
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-22 12:29:29 +0000

    putchar error

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,17 +9,13 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	int d;
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0, j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j] + '0');
-			if (j == 8)
-			{
-				_putchar('\n');
-			}
+			_putchar(*(*(i + a) + j));
 		}
+		_putchar('\n');
 	}
 }
