5b8c72bf576e3038bdb8432d32552cd7884b68f7
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-24 11:46:26 +0000

    strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,13 +9,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (!*s)
 	{
 		return (0);
 	}
-
-	if (*s != '\0')
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
+	return (1 + _strlen_recursion(++s));
 }
