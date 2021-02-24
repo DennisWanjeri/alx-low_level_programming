823d153fa157059848c5ea67f370a200b38a9f4b
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-24 16:05:48 +0000

    debug

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -10,16 +10,21 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0')
 	{
-		return (1);
-	}
-	if (*s1 && *s2)
-	{
-		if (*s1 == *s2 || *s1 == '*')
+		if (*s2 != '\0' && *s2 == '*')
 		{
-			return (wildcmp(++s1, ++s2));
+			return (wildcmp(s1, s2 + 1));
 		}
-		return (0);
+		return (*s2 == '\0');
 	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 2));
+	}
+	return (0);
 }
