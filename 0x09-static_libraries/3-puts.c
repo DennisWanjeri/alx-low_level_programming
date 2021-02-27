5c0bfcec7db075ed2a7dd17fd3ea564bf60c24f4
DennisWanjeri <shikandadennis07@gmail.com>
2021-02-27 07:14:56 +0000

    c files

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_puts - prints a string + newline
+ *
+ *@str:input
+ *
+ *Return:void
+ *
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
