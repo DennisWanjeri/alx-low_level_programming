f1597317e41b421ada5c5dce32fe155c30791f55
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-02 16:37:33 +0000

    debug

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -11,4 +11,37 @@
  */
 char *argstostr(int ac, char **av)
 {
-	
+	int i = 0, j = 0, z = 0, length = 0;
+
+	char *p;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			length++;
+		}
+		length += 1;
+	}
+	p = malloc((sizeof(char) * length) + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			p[z] = av[i][j];
+			z++;
+		}
+		p[z] = '/n';
+		z++;
+	}
+	p[z] = '\0';
+	return (p);
+}
