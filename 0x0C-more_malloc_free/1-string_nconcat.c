ffa8cb4c496ce925d8441cc9f8e1c608fd886a0b
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-04 10:47:25 +0000

    debug

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	length = len1 + n + 1;
 	p = malloc(sizeof(char) * length);
-	if (p == NULL))
+	if (p == NULL)
 	{
 		return (NULL);
 	}
