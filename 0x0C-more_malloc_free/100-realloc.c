7c52141a3fba9a541666d7080b1b16b41ddb33bf
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-04 13:44:58 +0000

    debugged

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	unsigned char *p;
 
-	if (new_size == 0 && ptr == NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
