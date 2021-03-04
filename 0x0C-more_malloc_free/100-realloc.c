edd6e24aab49161af0e031835dee7d3bc27ce3ce
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-04 13:39:14 +0000

    betty

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,12 +12,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	unsigned char *p;
 
-	if (new_size == 0 && ptr != NULL)
-        {
-                free(ptr);
-                return (NULL);
-        }
-
+	if (new_size == 0 && ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (new_size > old_size)
 	{
 		p = malloc(new_size);
