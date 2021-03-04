c1bbdd342a1d167cc2ac5c87f4aa9779060e27bb
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-04 13:34:15 +0000

    100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,4 +9,36 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	
+	unsigned int i;
+	unsigned char *p;
+
+	if (new_size == 0 && ptr != NULL)
+        {
+                free(ptr);
+                return (NULL);
+        }
+
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < old_size; i++)
+		{
+			p[i] = ((char *)ptr)[i];
+		}
+		free(ptr);
+		return (p);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+	}
+	return (p);
+}
