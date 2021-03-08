de840d0f4039977a992ab19701f371e3412b3230
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-08 10:05:10 +0000

    debug

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,5 +12,8 @@ typedef struct dog
 	char * name;
 	float age;
 	char *owner;
-};
+} dog_t;
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
