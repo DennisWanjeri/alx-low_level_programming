ef865c5ed200fbed86131bc0609b38152e6f24fa
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-08 12:54:07 +0000

    debug

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,11 +15,11 @@ void print_dog(struct dog *d)
 	}
 	if ((*d).name == NULL)
 	{
-		printf("Name: (nil)\n");
+		(*d).name = "(nil)";
 	}
 	if ((*d).owner == NULL)
 	{
-		printf("Owner: (nil)\n");
+		(*d).owner = "(nil)";
 	}
 	printf("Name: %s\n", (*d).name);
 	printf("Age: %f\n", (*d).age);
