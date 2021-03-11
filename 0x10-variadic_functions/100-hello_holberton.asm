ebb40dd1b0c0df0a50cfa477889806de193f5336
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-11 16:02:17 +0000

    debugging asm

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,14 @@
-section .data
+section .text
+	global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall
 
-	    message db "Hello, Holberton", 10
-
-	section .text
-
-	global _start
-_start:
-	    mov rax, 1
-	    mov rdi, 1
-	    mov rsi, message
-	    mov rdx, 14
-	    syscall
+	mov rax, 60
+	xor rdi, rdi
+	syscall
+	section .data
+message:	db "Hello, Holberton", 0xA, 0x0
