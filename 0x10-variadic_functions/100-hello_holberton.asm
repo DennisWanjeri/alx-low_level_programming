6f6d951015e894c38a8313658f82529378bc08ad
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-11 15:52:54 +0000

    assembly code

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,13 @@
+section .data
+
+	    message db "Hello, Holberton", 10
+
+	section .text
+
+	global _start
+_start:
+	    mov rax, 1
+	    mov rdi, 1
+	    mov rsi, message
+	    mov rdx, 14
+	    syscall
