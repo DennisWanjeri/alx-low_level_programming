4c0f4c3b9f4ad1002f49bc2900f773dc12c525ef
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-11 10:20:15 +0000

    debug

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,13 +15,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list va;
 	unsigned int i = 0;
 
-	if (separator == NULL)
-		separator = "";
 	va_start(va, n);
-	for (i = 0; i < (n - 1); i++)
+	for (i = 0; i <= (n - 1); i++)
 	{
-		printf("%d%s", va_arg(va, int), separator);
+		printf("%d", va_arg(va, int));
+		if (i < (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
 	}
-	printf("%d\n", va_arg(va, int));
+	printf("\n");
 	va_end(va);
 }
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ *print_all - prints any type of character
+ *
+ *@format:format
+ *
+void print_all(const char * const format, ...)
