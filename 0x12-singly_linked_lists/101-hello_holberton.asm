a9ff1aff5c6f1d70fce375dc63431da3330176ce
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-18 12:20:35 +0000

    debug

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,13 @@
-global main
-	extern printf
-	section .text
-main:	push rbx
-	lea
+	global	main
+		extern	printf
+		section	.text
+main:	push		rbx
+		lea
 	rdi, [rel message]
-	call printf
-	xor eax,eax
-	pop rbx
-	ret
-	section .data
+		call	printf
+		xor			eax,eax
+		pop		rbx
+		ret
+		section	.data
 message:	db
-	"Hello, Holberton", 10, 0
+"Hello, Holberton", 10, 0
