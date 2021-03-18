7606c590e740812af539824b47afdf08a78c210c
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-18 11:15:55 +0000

    string before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void startupfun (void) __attribute__ ((constructor));
+/**
+ *startupfun - prints a string before calling main
+ *Return:void
+ */
+void startupfun(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
