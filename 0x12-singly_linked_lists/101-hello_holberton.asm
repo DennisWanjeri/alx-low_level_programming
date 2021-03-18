ec7d99c67c4dffb9a47d0810716bf230d888ff02
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-18 12:52:49 +0000

    wow!

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,13 @@
-	global	main
-		extern	printf
-		section	.text
-main:	push		rbx
-		lea
-	rdi, [rel message]
-		call	printf
-		xor			eax,eax
-		pop		rbx
-		ret
-		section	.data
-message:	db
-"Hello, Holberton\n", 10, 0
+global main
+	extern printf
+
+	section .text
+
+main:
+	mov rdi, format
+	mov rax, 0
+	call printf
+	mov rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
