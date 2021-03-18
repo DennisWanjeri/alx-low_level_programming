fbc9aee3cbb436b57eb63b0ad948f8c51bbff4ff
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-18 11:23:04 +0000

    betty error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,5 +6,6 @@ void startupfun(void) __attribute__ ((constructor));
  */
 void startupfun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house");
+	printf("pon my back!\n");
 }
