5cefea12271d4a04702d3727c2c0f0252816e857
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-22 11:14:26 +0000

    get int at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *get_nodeint_at_index - gets a node at a certain index
+ *@head:pointer to listint
+ *@index:whose node we will return
+ *Return:pointer to node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
