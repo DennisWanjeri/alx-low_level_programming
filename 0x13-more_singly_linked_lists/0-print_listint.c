1ab44c8f953c87ff0fbad5597e4635340b4cddc4
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-23 07:10:10 +0000

    debug

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	unsigned int i;
+	int i;
 
 	if (h == NULL)
 		return (0);
-
-	while (h != NULL)
+	for (i = 0; h; i++)
 	{
 		printf("%d\n", h->n);
-		i++;
 		h = h->next;
 	}
 	return (i);
