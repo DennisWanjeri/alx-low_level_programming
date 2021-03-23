beb3f9e5106381e82d81e2ea80eb4fd0be908f0f
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-23 07:48:16 +0000

    debug

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,12 +7,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *d;
+	listint_t *temp;
 
-	while (head != NULL)
+	if (head == NULL)
+		return;
+	for (temp = head; temp; temp = temp->next)
 	{
-		d = head;
-		head = head->next;
-		free(d);
+		free(temp);
 	}
 }
