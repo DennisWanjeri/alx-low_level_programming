7d98040ebef4867e875e6cbc1981919a90b4af3f
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-23 06:47:01 +0000

    deletenode at index and correction of betty errors

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *delete_nodeint_at_index - deletes a node at a certain index
+ *@head:pointer to the nodeint list
+ *@index:index to delete at
+ *Return:(1) - success (-1) - otherwise
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp;
+
+	temp = *head;
+	if (i == index)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	while (*head)
+	{
+		if (i == index)
+		{
+			*head = tmp->next;
+			free(tmp);
+			return (1);
+		}
+		i++;
+	}
+	return (-1);
+}
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ *sum_listint - gets the sum of the data of a listint list
+ *@head:pointer to list
+ *Return:sum
+ */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
