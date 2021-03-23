6e4b06374e9fd1fbba81dc0f20cd62b88bcaf394
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-23 06:20:20 +0000

    insert node at idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,3 +9,41 @@
  *Return:pointer to the new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new = malloc(sizeof(listint_t)), *prev;
+	int i = 0;
+	listint_t *temp;
+
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	temp = *head;
+	new->n = n;
+	if (i == idx)
+	{
+		new->next = temp;
+		return (new);
+	}
+	while (temp->next)
+	{
+		if (i == idx)
+		{
+			prev->next = new;
+			new->next = temp;
+			return (new);
+		}
+		i++;
+		prev = temp;
+		temp = temp->next;
+/*when the idx points to the end*/
+		if (i == idx && !temp)
+		{
+			prev->next = new;
+			new->next = NULL;
+			return (new);
+		}
+	}
+	return (NULL);
+}
