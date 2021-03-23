3f4f162e65ae6bfb527c115bee01292547707db6
DennisWanjeri <shikandadennis07@gmail.com>
2021-03-23 08:28:38 +0000

    debug

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new = malloc(sizeof(listint_t)), *prev;
-	int i = 0;
+	unsigned int i = 0;
 	listint_t *temp;
 
 	if (new == NULL)
