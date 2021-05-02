93009467a5fa0ae34035549f8fb1dca217432bbd
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-02 15:13:10 +0000

    inserted missing parantheses

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int n = 0;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 	while (head->prev)
 		head = head->prev;
 	while (head)
