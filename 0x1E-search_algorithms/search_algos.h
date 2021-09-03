dafaba0a4f1264bcf00afc149f7810756a89ea0c
DennisWanjeri <shikandadennis07@gmail.com>
2021-09-03 02:58:07 +0000

    big O

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -47,23 +47,4 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 skiplist_t *linear_skip(skiplist_t *list, int value);
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
-typedef struct skiplist_s
-{
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
-} skiplist_t;
-skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif /*SEARCH_ALGOS_H*/
